4cc95e3a47881a0d0860a6a4cbdbb593f2509927
patd <pdoran@gmail.com>
2023-01-29 18:07:11 -0500

    Add assets directory and images

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,8 @@ module.exports = function (eleventyConfig) {
     // Copy the `css` directory to the output
     eleventyConfig.addPassthroughCopy('css');
   
+    eleventyConfig.addPassthroughCopy('assets');
+    
     // Watch the `css` directory for changes
     eleventyConfig.addWatchTarget('css');
 
