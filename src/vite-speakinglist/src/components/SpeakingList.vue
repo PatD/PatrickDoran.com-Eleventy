5a89ec5e9d240ec934aaef913395d2ff46b9f474
patd <pdoran@gmail.com>
2023-03-10 08:49:46 -0500

    Add Vue component

---
+++ src/vite-speakinglist/src/components/SpeakingList.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+//   msg: String,
+})
+</script>
+
+<template>
+    <div>
+        <h2>speaking list components</h2>
+    </div>
+</template>
